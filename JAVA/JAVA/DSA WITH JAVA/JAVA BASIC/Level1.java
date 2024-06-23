e254fd808dd49e80c1363fdc710d6cf22407e940
navnathdeshmukh45 <navnathdeshmukh363@gmail.com>
2024-06-23 23:54:39 +0530

    commit

---
+++ JAVA/JAVA/DSA WITH JAVA/JAVA BASIC/Level1.java
@@ -0,0 +1,52 @@
+public class Level1{
+    public static void main(String[] arg){
+        System.out.println(0);
+
+        //To data types in java 
+        int a =34;
+        System.out.println(a);
+        double b = 4.5;
+        System.out.println(b);
+        float c=19.0f;
+        System.out.println(c);
+        char d='d';
+        System.out.println(d);
+        String  s ="say";
+        System.out.println(s);
+        //boolean h ="true";
+       //System.out.println(h);
+
+        //Type casting
+        int myint = 9;
+        double mydouble = myint;
+        System.out.println(mydouble);
+
+        //AO
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+
+        //AO
+        b=a;
+        System.out.println(b);
+        b=+a;
+        System.out.println(b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ PYTHON WITH DSA/PYTHON PROGARMMING/Basic level.py
@@ -0,0 +1,18 @@
+# To Print Simple Statement
+print("Hello Coder")
+
+# To Take Input 
+# n = input("Enter The Some Word: ")
+
+# Conditional statement
+a = 67
+b = 679
+if a > b :
+    print("true")
+else:
+    print("false")
+
+# for loop
+for a in range(1,100):
+    print(a)
+
